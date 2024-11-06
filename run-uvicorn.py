c69bb23d11f1f996982e4c8b1d474403534e29c7
RSeauberlich <richard.saeuberlich@i3denergy.de>
2024-11-06 21:27:11 +0100

    change to working example

--- run-uvicorn.py
+++ run-uvicorn.py
@@ -1,11 +1,18 @@
 __author__ = 'Richard Saeuberlich'
 
+import os
+
 import uvicorn
+import django
 
 from solo_asynchronous.constants import set_uvicorn_runner
 
 if __name__ == "__main__":
     set_uvicorn_runner(True)
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solo_asynchronous.settings')
+    django.setup()
+
     # @see https://www.uvicorn.org/#running-programmatically
     config = uvicorn.Config('solo_asynchronous.asgi:application', workers=1)
     server = uvicorn.Server(config)
--- solo_asynchronous/asgi.py
+++ solo_asynchronous/asgi.py
@@ -6,24 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
-import os
-import django
-
-from channels.routing import get_default_application, ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-from solo_asynchronous.constants import get_uvicorn_runner
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solo_asynchronous.settings')
-
-if get_uvicorn_runner():
-    # Guvicorn
-    django.setup()
-    application = get_default_application()
-else:
-    # Daphne
-    asgi_application = get_asgi_application()
-    application = ProtocolTypeRouter({
-        'http': asgi_application,
-    })
+asgi_application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': asgi_application,
+})
